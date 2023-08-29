fcc5e05d75bb94733b613cc617b9b016afba2a80
Allan <132112000+Wynso-42@users.noreply.github.com>
2023-08-29 18:04:18 +0200

    Update ft_ultimate_range.c

--- c07/ex02/ft_ultimate_range.c
+++ c07/ex02/ft_ultimate_range.c
@@ -15,21 +15,21 @@
 
 int	ft_ultimate_range(int **range, int min, int max)
 {
-	int	bound; // stock la taille du tableau //
-	int	index; // parcours le tableau //
-	int	*buffer; // alloue la memoire au tableau //
+	int	bound;
+	int	index;
+	int	*buffer;
 
-	if (min >= max) // verifie si 'min' est sup ou egal a 'max', si oui, la fonction met le pointeur 'range' à NULL et renvoie 0.
+	if (min >= max)
 	{
 		*range = 0;
 		return (0);
 	}
-	bound = max - min; // calcul de la taille du tableau en soustrayant min de max, ca donne la quantite d'elements dans la sequence //
-	buffer = malloc(bound * sizeof(int)); // alloue la memoire au tableau, egal a 'bound' multiplie par la taille de l'int//
-	if (!buffer) // verifie si l'allocation de memoire a reussie //
+	bound = max - min;
+	buffer = malloc(bound * sizeof(int));
+	if (!buffer)
 	{
 		*range = 0;
-		return (-1); // si buffer est null, la memoire n'a pas ete allouee, met le pointeur 'range' a NULL et donc renvoie '-1' pour indiquer une erreur //
+		return (-1);
 	}
 	*range = buffer;
 	index = 0;
@@ -41,8 +41,7 @@ int	ft_ultimate_range(int **range, int min, int max)
 	return (bound);
 }
 
-/*
-int    main(void)
+/*int    main(void)
 {
     int    min;
     int    max;
@@ -59,5 +58,4 @@ int    main(void)
         i++;
     }
 
-}
-*/
+}*/
