c3019c04b52ca1571363aefe549f01a16b27771d
Allan <132112000+Wynso-42@users.noreply.github.com>
2023-08-29 13:22:58 +0200

    Update ft_strjoin.c

--- c07/ex03/ft_strjoin.c
+++ c07/ex03/ft_strjoin.c
@@ -12,30 +12,74 @@
 
 #include <stdlib.h>
 
-char	*ft_strjoin(int size, char **strs, char *sep)
+int	ft_strlen(char *str)
 {
-	char	*s;
-	int		i;
-	int		j;
-	int		c;
+	int	i;
 
-	s = malloc(sizeof(strs));
 	i = 0;
-	c = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	get_malloc_size(int size, char **strs, char *sep)
+{
+	int	i;
+	int	res;
+
+	if (size == 0)
+		return (1);
+	i = 0;
+	res = 0;
 	while (i < size)
+		res += ft_strlen(strs[i++]);
+	res += ft_strlen(sep) * (size - 1);
+	return (res + 1);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	destlen;
+
+	i = 0;
+	destlen = ft_strlen(dest);
+	while (src[i])
 	{
-		j = 0;
-		while (strs[i][j] != '\0')
-		{
-			s[c++] = strs[i][j++];
-		}
-		j = 0;
-		while (sep[j] != '\0' && i != size - 1)
-		{
-			s[c++] = sep[j++];
-		}
+		dest[destlen + i] = src[i];
 		i++;
 	}
-	s[c] = '\0';
-	return (s);
+	dest[destlen + i] = 0;
+	return (dest);
 }
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	char	*res;
+	int		i;
+
+	res = malloc(get_malloc_size(size, strs, sep) * sizeof(char));
+	if (!res)
+		return (NULL);
+	res[0] = 0;
+	if (size == 0)
+		return (res);
+	i = 0;
+	while (i < size)
+	{
+		ft_strcat(res, strs[i]);
+		if (i != size - 1)
+			ft_strcat(res, sep);
+		i++;
+	}
+	res[ft_strlen(res)] = 0;
+	return (res);
+}
+
+/*#include <stdio.h>
+
+int main(){
+    char    *tab[] = {"salut","tu","vas","bien","?"};
+    char *r = ft_strjoin(5, tab, " ");
+    printf("%s",r);
+}*/
