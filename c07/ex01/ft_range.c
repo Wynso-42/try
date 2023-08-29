f5dc665ecf07b7d943c565de1362b1298fb74b6e
Allan <132112000+Wynso-42@users.noreply.github.com>
2023-08-29 18:03:53 +0200

    Update ft_range.c

--- c07/ex01/ft_range.c
+++ c07/ex01/ft_range.c
@@ -15,25 +15,24 @@
 
 int	*ft_range(int min, int max)
 {
-	int	range; // stock la diff entre max et min //
-	int	index; // parcours le tableau //
-	int	*buffer; // pointeur vers le tableau //
+	int	range;
+	int	index;
+	int	*buffer;
 
-	if (min >= max) 
+	if (min >= max)
 		return (0); 
-	range = max - min; // Calcule la plage des nombres //
-	buffer = malloc(range * sizeof(int)); // alloue la memoire pour le tableau, egal a 'range' et multiplie par la taille de l'int //
-	if (!buffer) // verifie si l'allocation de memoire a reussie //
-		return (0); // si buffer est nul, la memoire n'a pas ete allouee et donc renvoie null //
+	range = max - min;
+	buffer = malloc(range * sizeof(int));
+	if (!buffer)
+		return (0);
 	index = 0;
 	while (index < range)
 	{
-		buffer[index] = min + index; // on remplie avec des valeurs en ajoutant 'min' a l'index actuel, ca crée une séquence de nombres allant de 'min' à 'max' '-1'//
-		index++; // passe a la prochaine position du tableau // 
+		buffer[index] = min + index;
+		index++;
 	}
-	return (buffer); // renvoie au debut du tableau //
+	return (buffer);
 }
-
 
 /*int        main(void)
 {
