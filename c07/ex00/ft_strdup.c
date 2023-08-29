941eae1277fad1680ac2220987c7c7130074a65b
Allan <132112000+Wynso-42@users.noreply.github.com>
2023-08-29 18:03:26 +0200

    Update ft_strdup.c

--- c07/ex00/ft_strdup.c
+++ c07/ex00/ft_strdup.c
@@ -11,34 +11,42 @@
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
 
-int ft_str_length(char *str)
+int	ft_str_length(char *str)
 {
-    int index; // Utilisé pour parcourir le tableau
+	int	index;
 
-    index = 0; // Initialisation à 0
-    while (str[index]) // Tant que le caractère actuel n'est pas '\0'
-        index++; // Augmente l'indice pour passer au caractère suivant
-    return (index); // Renvoie la longueur totale de la chaîne
+	index = 0;
+	while (str[index])
+		index++;
+	return (index);
 }
 
-char *ft_strdup(char *src)
+char	*ft_strdup(char *src)
 {
-    int index;
-    char *dest;
-    char *d;
+	int		index;
+	char	*dest;
+	char	*d;
 
-    index = 0;
-    d = ((dest = (char *)malloc(ft_str_length(src) * sizeof(char) + 1))); // Alloue de la mémoire pour la nouvelle chaîne
-    if (!d) // Vérifie si l'allocation a réussi
-    {
-        return (0); // Si non, renvoie 0 pour indiquer une erreur
-    }
-    while (src[index]) // Tant que le caractère actuel de la source n'est pas '\0'
-    {
-        dest[index] = src[index]; // Copie le caractère de la source vers la destination
-        index++; // Passe au caractère suivant
-    }
-    dest[index] = '\0'; // Ajoute le caractère de fin de chaîne à la destination
-    return (dest); // Renvoie la nouvelle chaîne dupliquée
+	index = 0;
+	d = ((dest = (char *)malloc(ft_str_length(src) * sizeof(char) + 1)));
+	if (!d)
+	{
+		return (0);
+	}
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	dest[index] = '\0';
+	return (dest);
 }
+
+/*int main ()
+{
+    char *src = "Hello World !";
+    char *dup = ft_strdup(src);
+    printf("%s", dup);
+}*/
